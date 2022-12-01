1e55a80a9c3bf615e0b3baebd9807f3951fc16b8
ElijahPeraton <elijah.hyndman@peraton.com>
2022-12-01 10:38:35 -0500

    first Commit. Completed Day 1 Part 1

---
+++ 1Day/Main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define INPUT_PATH "./input.txt"
+
+#define END_LINE 0 // MUST be 0000
+#define END_FILE -1
+
+
+/*
+* Helper Function
+*/
+int getCalorie(std::ifstream& f) {
+  // Case: end of file
+  if(!f.good())
+    return END_FILE;
+
+  try {
+    // Case: calories to count
+    std::string in;
+    std::getline(f, in);
+    return stoi(in);
+  } catch (...) {
+    // case: no calories; failure to integer-convert '\n'
+    return END_LINE;
+  }
+}
+
+
+int main() {
+  // Access File
+  std::ifstream f(INPUT_PATH);
+  std::string in;
+
+  if(!f.is_open())
+    return -1;
+
+  // Parse
+  int sum=0, max=0;
+  for(int c; c!=END_FILE; c=getCalorie(f)) {
+      if(c) sum += c;
+      else {
+        max = (sum>max) ? sum : max;
+        sum = 0;
+      }
+  }
+
+  printf("max calories: %d\n", max);
+
+  return 0;
+}
